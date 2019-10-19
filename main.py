4260ced2fc20b6f1fc77ccc0a6e7fa2ef746a153
Miguel Gomez <mgomez.developer@gmail.com>
2019-10-19 10:30:01 -0400

    Comments are added for the blogz

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:launchcode@localh
 # Print ORM commands in console
 app.config['SQLALCHEMY_ECHO'] = True
 
-app.secret_key = 'ioga;skebr'
+app.secret_key = '71408a9f3f75d712b98d4f20bad52'
 
 # MYSQL DB Connection
 db = SQLAlchemy(app)
@@ -36,6 +36,8 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
 
+    # Create a foreign key that connects User class to 
+    # Blog class. (one User can have many Posts). 
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body, owner):
@@ -43,6 +45,10 @@ class Blog(db.Model):
         self.body = body
         self.owner = owner
 
+# TODO
+# Create a User model with some starting fields
+# that include username and passkey. Create a 
+# relationship that points to tne owner field
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -50,97 +56,143 @@ class User(db.Model):
     pw_hash = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
+    # Encrypt the password by using one of the functions
+    # in hashutils file, and store it in passkey field
     def __init__(self, username, password):
         self.username = username
         self.pw_hash = make_pw_hash(password)
 
 
+# TODO
+# After User has created an account. User can see
+# all the blog Posts published and authors page.
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     
+    # When User submits Login form
     if request.method == 'POST':
-
+        # Store the fields for each User
         username = request.form['username']
         password = request.form['password']
 
+        # Access the User stored in the Database by 
+        # searching for the username
         user = User.query.filter_by(username=username).first()
 
+        # if the Username does not exist
         if user == None:
-            flash('Username does not exists', 'user')
+            flash('Username could not be found', 'user')
+
             return render_template('login.html')
 
+        # If user leaves a blank password    
         if password == '':
-            flash('Password does not exists', 'pwd')
+            flash('Cannot leave password blank', 'pwd')
+            
             return render_template('login.html')
         
+        # If the password enter does not match has stored in
+        # the Database
         elif not check_pw_hash(password, user.pw_hash):
-            flash('User password is incorrect', 'pwd')
+            flash('Password typed is incorrect', 'pwd')
+            
             return render_template('login.html')
 
+        # Login validation passes
         else:
+            # Store each username in a cookie session
             session['username'] = username
+
+            # Redirect User to published posts page
             return redirect('/blog_posts')
 
     return render_template('login.html')
 
 
+# TODO
+# Guest User needs to create an account, 
+# in order to create a New Post 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
-
+    # When User submits Signup form
     if request.method == 'POST':
-
+        # Access the fields of Signup form
         username = request.form['username']
         pwd = request.form['password']
         verify = request.form['verify']
 
+        # Query the Database to compare uf User 
+        # already exists. Grab the first record
         user = User.query.filter_by(username=username).first()
 
+        # If a User has already register with that username
         if user != None:
             if user.username == username:
-                flash('This username already exists')
-
+                flash('That username has already been taken')
+        
+        # If User leaves username field blank 
         elif username == '':
-            flash('Username must be filled in')
-         
+            flash('Username field cannot be blank')
+        
+        # If username is not a least 3 characters 
         elif len(username) < 3:
             flash('Username need to be at least 3 characters')
          
+        # if User leaves password blank
         elif pwd == '':
-            flash('Password need to be filled in')
-            
+            flash('Password field cannot be blank')
+
+        # If password is not at least 3 characters
         elif len(pwd) < 3:
             flash('Password needs to be at least 3 characters')
 
+        # If User leaves Confirm Password field blank
         elif verify == '':
-            flash('Confirm Password need to be filled in')
-
+            flash('Confirm Password cannot be blank')
+        
+        # If Password enter is not equal Confirm password 
         elif pwd != verify:
-            flash('Passwords do not match')
-      
+            flash('Password entered does not match')
+        
+        # Registration validation passes
         else:
+            # Create the new User registered and 
+            # stored it in MySQL Database
             new_user = User(username, pwd)
             db.session.add(new_user)
             db.session.commit()
-         
+
+            # Keep the session of username
+            # registered and redirect User
+            # to the create post page.
             session['username'] = username
+
             return redirect('/newpost')
 
     return render_template('signup.html')
 
 
+# TODO
+# 
 @app.route('/logout')
 def logout():
-
+    
+    # Check User session is active 
+    # before deleting session cookie
     if session:
         del session['username']
     
     return redirect('/blog_posts')
 
-
+# TODO
+# Users must login in order to create an 
+# unlimited number of Posts
 @app.before_request
 def require_login():
+    # Public routes guests can browse
     allowed_routes = ['index','login', 'blog_posts', 'signup']
-
+    
+    # Restrict guests from unauthorized pages
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
@@ -197,21 +249,33 @@ def create_post():
     
     return render_template('create_post.html')
 
+# TODO
+# Change route so that a decision can be made when blog posts 
+# page needs to display dynamically all registered users, a 
+# template for each individual post, or all the blog posts.
 
-
-# Same URL address for home and page to display blog posts
 @app.route('/blog_posts', methods=['POST', 'GET'])
 def blog_posts():
 
+    # TODO
+    # Use request.args.get to store the parsed contents 
+    # that comes from Post and User table string query
+
+    # post id that belongs to each Post 
     post_id = request.args.get('id')
+    # user id that belongs to each User
     user_id = request.args.get('user')
 
+    # If author is found
     if user_id:
+        # Access all the authors that has written a Post
         writers = Blog.query.filter_by(owner_id=user_id).all()
 
         return render_template('blog_posts.html',user=writers, authors=user_id)
 
+    # if post is found 
     elif post_id:
+        # Access each individual Post written by author
         posts = Blog.query.filter_by(id=post_id).all()
 
         return render_template('blog_posts.html', post=posts, page=post_id)     
@@ -223,15 +287,20 @@ def blog_posts():
         # Render blog posts page with each single post 
         return render_template('blog_posts.html', posts=posts)
 
-
+# TODO
+# User can browse blog Posts that belong to each author
+# as he or she clicked on the corresponding username
 @app.route('/')
 def index():
 
+    # In this case, Users stored in the Database
+    # are all authors. So all of them are display
     users = User.query.all()
-    return render_template('index.html', users=users)
 
+    return render_template('index.html', users=users)
 
 
 # This is the main file to run the app
 if __name__ == "__main__":
     app.run()
+
